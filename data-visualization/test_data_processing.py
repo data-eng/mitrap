ee30ce77acc6cbfd57f2f3d01e5c8f5cc5b73748
tatiana-boura <tat.boura@gmail.com>
2024-10-14 13:04:29 +0300

    Add string info to different measurement

--- data-visualization/test_data_processing.py
+++ data-visualization/test_data_processing.py
@@ -19,11 +19,11 @@ df.columns = df.columns.str.replace(' ', '_').str.lower()
 df.columns = df.columns.str.replace(r'\s*\(.*?\)', '', regex=True).str.strip()
 df.columns = df.columns.str.rstrip('_')
 
-print(df.columns)
+print(df.dtypes)
 
 df.to_csv('data/MA200_filtered.csv')
 
-families = {'flows': ['flow_setpoint', 'flow_total', 'flow1', 'flow2'],
+'''families = {'flows': ['flow_setpoint', 'flow_total', 'flow1', 'flow2'],
             'sample': ['sample_temp', 'sample_rh', 'sample_dewpoint'],
             'uv': ['uv_bc1', 'uv_bc2', 'uv_bcc', 'uv_atn1', 'uv_atn2', 'uv_k'],
             'ir': ['ir_atn1', 'ir_atn2', 'ir_k', 'ir_bc1', 'ir_bc2', 'ir_bcc'],
@@ -31,7 +31,20 @@ families = {'flows': ['flow_setpoint', 'flow_total', 'flow1', 'flow2'],
             'green': ['green_atn1', 'green_atn2', 'green_k', 'green_bc1', 'green_bc2', 'green_bcc'],
             'red': ['red_atn1', 'red_atn2', 'red_k', 'red_bc1', 'red_bc2', 'red_bcc'],
             'info': ['gps_lat', 'gps_long', 'status', 'battery_remaining', 'tape_position', 'readable_status'],
-            'internal': ['internal_pressure', 'internal_temp']}
+            'internal': ['internal_pressure', 'internal_temp']}'''
+
+
+families = {'flows': ['flow_setpoint', 'flow_total', 'flow1', 'flow2'],
+            'sample': ['sample_temp', 'sample_rh', 'sample_dewpoint'],
+            'uv': ['uv_bc1', 'uv_bc2', 'uv_bcc', 'uv_atn1'],
+            'ir': ['ir_atn1',  'ir_bc1', 'ir_bc2', 'ir_bcc'],
+            'blue': ['blue_atn1', 'blue_bc1', 'blue_bc2', 'blue_bcc'],
+            'green': ['green_atn1', 'green_bc1', 'green_bc2', 'green_bcc'],
+            'red': ['red_atn1',  'red_bc1', 'red_bc2', 'red_bcc'],
+            'info': ['gps_lat', 'gps_long', 'status', 'battery_remaining', 'tape_position'],
+            'internal': ['internal_pressure', 'internal_temp'],
+            'string_values': ['uv_atn2', 'uv_k', 'blue_atn2', 'blue_k', 'green_atn2', 'green_k', 'red_atn2', 'red_k',
+                              'ir_atn2', 'ir_k', 'readable_status']}
 
 
 influx_lines = []
@@ -58,7 +71,7 @@ print("\nInfluxDB Line Protocol Format:")
 '''for line in influx_lines:
     print(line)'''
 
-with open('C:/Users/tatbo/Desktop/influx_csv.txt', 'w', newline='') as file:
+with open('data/influx_csv.txt', 'w', newline='') as file:
     for line in influx_lines:
         file.write(f"{line}\n")
 
