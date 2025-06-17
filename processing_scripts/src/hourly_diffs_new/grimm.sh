78e8ad9ba4e3bd81b930901cfc0b962bcf8e1cf1
Stasinos Konstantopoulos <konstant@iit.demokritos.gr>
2025-06-17 23:54:38 +0300

    Rework the processor

--- processing_scripts/src/hourly_diffs_new/grimm.sh
+++ processing_scripts/src/hourly_diffs_new/grimm.sh
@@ -17,14 +17,13 @@ fi
 # See in sync.sh how OLDLINES and NEWLINES are computed and used to
 # decide what to transfer to new/
 
-SPOOL=/home/konstant/projects/mitrap/temp/spool
+SPOOL=/mnt/spool
 
 station=$1
 file_to_process=$2
 timestamp_DD=$3
 file_to_store=$4
-#dir_influx_log="/home/debian/src/mitrap/influx_log/$timestamp_DD/$station"
-dir_influx_log="/home/konstant/projects/mitrap/temp/$timestamp_DD/$station"
+dir_influx_log="/home/debian/src/mitrap/influx_log/$timestamp_DD/$station"
 mkdir -p $dir_influx_log
 
 PLINES=$(grep -n '^P' $file_to_process | cut -d: -f 1)
@@ -50,16 +49,17 @@ fi
 cat $file_to_process | head -n $((FIRSTPLINE - 1)) >> ${SPOOL}/grim
 if [[ $(cat ${SPOOL}/grim | wc -l) == 41 ]]; then
     echo "Spool looks good. Kept."
-    mv ${SPOOL}/grim ${dir_influx_log}/file
+    mv ${SPOOL}/grim ${file_to_process}.temp
 else
     echo "Bad spool. Deleted."
     rm ${SPOOL}/grim
+    rm -f ${file_to_process}.temp
 fi
 
 # The useful data is the lines between the first PLINE and one line
 # before the last PLINE
 
-cat $file_to_process | head -n $((LASTPLINE - 1)) | tail -n +${FIRSTPLINE} >> ${dir_influx_log}/file
+cat $file_to_process | head -n $((LASTPLINE - 1)) | tail -n +${FIRSTPLINE} >> ${file_to_process}.temp
 
 # The next spool is the lines from the last PLINE onwards
 cat $file_to_process | tail -n +${LASTPLINE} > ${SPOOL}/tmp
@@ -68,13 +68,16 @@ NFULL=$(cat ${SPOOL}/tmp | wc -l)
 cat ${SPOOL}/tmp | head -n ${NFULL} > ${SPOOL}/grim
 
 
+mv ${file_to_process}.temp ${file_to_process}
+
+
 # Fixed column names: 
   # J :    0.25     0.28     0.30     0.35     0.40     0.45     0.50     0.58
   # J ;    0.65     0.70     0.80     1.00     1.30     1.60     2.0      2.5
 
 cols=(nm0_25 nm0_28 nm0_30 nm0_35 nm0_40 nm0_45 nm0_50 nm0_58 nm0_65 nm0_70 nm0_80 nm1_00 nm1_30 nm1_60 nm2_00 nm2_50)
 
-
+	
 while IFS= read -r line; do
 
     # If is P line make the timestamp
@@ -91,14 +94,7 @@ while IFS= read -r line; do
       date_str=$(printf "%04d-%02d-%02d %02d:%02d:%02d" "$year" "$mm" "$dd" "$HH" "$MM" "$SS")
       timestamp_unix=$(date -d "$date_str" +%s%N)
 
-      prev_c="none"
-
-      continue
-    fi
-
-    # If cC line then map each two lines to the corresponding column names.
-    # Also lowercase c only contains an extra column, which we remove
-    if [[ "$line" =~ ^[cC][0-9] ]]; then
+    else
 
       cleaned="${line//[:;]/}"
       read -ra values <<< "$cleaned"
@@ -106,34 +102,21 @@ while IFS= read -r line; do
       cname=${values[0]}
       values=("${values[@]:1}")
 
-      # For lowercase c ignore last value
-      if [[ "${cname:0:1}" == "c" ]]; then
-        unset 'values[-1]'
-      fi
+      fields=""
+      for i in "${!values[@]}"; do
+        val="${values[i]}"; col="${cols[i]}";
+        fields+=",${col}=${val}"
+      done
 
-      if [[ "$prev_c" != "$cname" ]]; then
-        fields_1=""
-        for i in "${!values[@]}"; do
-          val="${values[i]}"; col="${cols[i]}";
-          fields_1+=",${col}=${val}"
-        done
-      else
-        fields_2=""
-        for i in "${!values[@]}"; do
-          j=$((i+8))
-          val="${values[i]}"; col="${cols[j]}";
-          fields_2+=",${col}=${val}"
-        done
-
-        fields_2="${fields_2#,}"
-
-        # Influx line
-        write_query="grimm,name=${cname} ${fields_1} ${fields_2} ${timestamp_unix}" >> "$dir_influx_log/$file_to_store.txt"
-
-      fi
-
-      prev_c=$cname
+      # Influx line
+      write_query="grimm,name=${cname} ${fields} ${timestamp_unix}"
+      echo $write_query >> "$dir_influx_log/$file_to_store.txt"
 
     fi
 
-done < "$file_to_process"
+done < <( cat $file_to_process | gawk '/P/ { Q=0; print; } /^[Cc]/ { if (Q%4 == 0) { MYLINE = $1; for (i=2; i<NF; i++) MYLINE = MYLINE " " $i ; Q=Q+1 ; } else if  (Q%4==1) { for (i=2; i<NF; i++) MYLINE = MYLINE " " $i ; Q=Q+1; print MYLINE } else if  (Q%4==2) { MYLINE = $1; for (i=2; i<NF-1; i++) MYLINE = MYLINE FS $i ; Q=Q+1 } else if  (Q%4==3) { for (i=2; i<NF; i++) MYLINE= MYLINE " " $i ; Q=Q+1; print MYLINE } } ' )
+
+# The awk script (a) lets P lines fall through (b) collects pairs of Cc lines into one line
+# (c) drop the last element of the first line of a c pair (TODO: sanity check, must be 160)
+
+
