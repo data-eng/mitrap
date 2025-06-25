d2241ffd3446a82273a5bd2f7cd723158b650ee6
tatiana-boura <tat.boura@gmail.com>
2025-06-25 16:39:47 +0300

    Make all numbers floats for InfluxDB

--- processing_scripts/src/hourly_diffs_new/ae31.sh
+++ processing_scripts/src/hourly_diffs_new/ae31.sh
@@ -10,9 +10,19 @@ file_to_store=$2
 installation_name=$3
 instrument_name=$4
 
+regex='^-?[0-9]+(\.[0-9]+)?$'
+
 while IFS=',' read -r timestamp datestr timestr nm370 nm450 nm520 nm590 nm660 nm880 nm950 flow rest; do
 
     timestamp_unix=$(date -d "$timestamp" +%s)000000000
+
+    # Make integers float to avoid flux being quirky
+    for var in nm370 nm450 nm520 nm590 nm660 nm880 nm950; do
+      if [[ "${!var}" =~ ^-?[0-9]+$ ]]; then
+        printf -v "$var" '%s.0' "${!var}"
+      fi
+    done
+
 
     write_query='ae31,installation="'"$installation_name"'",instrument="'"${instrument_name}"'"'" date_str=$datestr,time_str=$timestr,nm370=$nm370,nm450=$nm450,nm520=$nm520,nm590=$nm590,nm660=$nm660,nm880=$nm880,nm950=$nm950,flow=$flow $timestamp_unix"
 
