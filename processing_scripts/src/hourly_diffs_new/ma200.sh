2637b8afd288d6a47e4b7e37acda66dacef2b53a
tatiana-boura <tat.boura@gmail.com>
2025-07-28 15:06:37 +0300

    MA200 correct variable handling

--- processing_scripts/src/hourly_diffs_new/ma200.sh
+++ processing_scripts/src/hourly_diffs_new/ma200.sh
@@ -36,11 +36,19 @@ do
   irbcc=$(printf "%d\n" "$irbcc")
 
   # Force integer-looking numbers to floats
-  for var in uvbc1 uvbc2 uvbcc bluebc1 bluebc2 bluebcc greenbc1 greenbc2 greenbcc redbc1 redbc2 redbcc irbc1 irbc2 irbcc; do
-    if [[ "${!var}" =~ ^-?[0-9]+$ ]]; then
-      printf -v "$var" '%s.0' "${!var}"
-    fi
-  done
+  for var in time serial session data_format firmware datetime offset ymd hms gps_lat gps_long gps_alt gps_speed gps_sat timebase status battery ax ay az tape flow_setpoint flow_total flow1 flow2 temp rh dew pressure int_temp opt_conf \
+  uv1 uv2 uvref uvatn1 uvatn2 uvk \
+  blue1 blue2 blueref blueatn1 blueatn2 bluek \
+  green1 green2 greenref greenatn1 greenatn2 greenk \
+  red1 red2 redref redatn1 redatn2 redk \
+  ir1 ir2 irref iratn1 iratn2 irk \
+  uvbc1 uvbc2 uvbcc bluebc1 bluebc2 bluebcc greenbc1 greenbc2 greenbcc redbc1 redbc2 redbcc irbc1 irbc2 irbcc
+do
+  if [[ "${!var}" =~ ^-?[0-9]+$ ]]; then
+    eval "$var=\"${!var}.0\""
+  fi
+done
+
 
   write_query="ma200,installation=$installation_name,instrument=$instrument_name flow_total=$flow_total,flow1=$flow1,flow2=$flow2,uv1=$uv1,uv2=$uv2,uvref=$uvref,uvatn1=$uvatn1,uvatn2=$uvatn2,uvk=$uvk,blue1=$blue1,blue2=$blue2,blueref=$blueref,blueatn1=$blueatn1,blueatn2=$blueatn2,bluek=$bluek,green1=$green1,green2=$green2,greenref=$greenref,greenatn1=$greenatn1,greenatn2=$greenatn2,greenk=$greenk,red1=$red1,red2=$red2,redref=$redref,redatn1=$redatn1,redatn2=$redatn2,redk=$redk,ir1=$ir1,ir2=$ir2,irref=$irref,iratn1=$iratn1,iratn2=$iratn2,irk=$irk,uvbc1=$uvbc1,uvbc2=$uvbc2,uvbcc=$uvbcc,bluebc1=$bluebc1,bluebc2=$bluebc2,bluebcc=$bluebcc,greenbc1=$greenbc1,greenbc2=$greenbc2,greenbcc=$greenbcc,redbc1=$redbc1,redbc2=$redbc2,redbcc=$redbcc,irbc1=$irbc1,irbc2=$irbc2,irbcc=$irbcc $timestamp_unix"
 
