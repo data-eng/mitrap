baa8aadb9cc71f7ead65f37954450672ef30fea0
tatiana-boura <tat.boura@gmail.com>
2025-07-30 15:56:06 +0300

    MA200 store only plot-relevant columns

--- processing_scripts/src/hourly_diffs_new/ma200.sh
+++ processing_scripts/src/hourly_diffs_new/ma200.sh
@@ -22,12 +22,8 @@ file_to_store=$2
 installation_name=$(escape_tag_value "$3")
 instrument_name=$(escape_tag_value "$4")
 
-while IFS=',' read -r date time serial session data_format firmware datetime offset ymd hms gps_lat gps_long gps_alt gps_speed gps_sat timebase status battery ax ay az tape flow_setpoint flow_total flow1 flow2 temp rh dew pressure int_temp opt_conf \
-  uv1 uv2 uvref uvatn1 uvatn2 uvk \
-  blue1 blue2 blueref blueatn1 blueatn2 bluek \
-  green1 green2 greenref greenatn1 greenatn2 greenk \
-  red1 red2 redref redatn1 redatn2 redk \
-  ir1 ir2 irref iratn1 iratn2 irk \
+while IFS=',' read -r date time v0 v1 v2 v3 v4 v5 v6 v7 v8 v9 v10 v11 v12 v13 v14 v15 v16 v17 v18 v19 v20 flow_total flow1 flow2 v21 v22 v23 v24 v25 v26 \
+  uv0 uv1 uv2 uv3 uv4 uvatn1 b1 b2 b3 b4 b5 blueatn1 g1 g2 g3 g4 g5 greenatn1 r0 r1 r2 r3 r4 redatn1 ir0 ir1 ir2 ir3 ir4 iratn1 \
   uvbc1 uvbc2 uvbcc bluebc1 bluebc2 bluebcc greenbc1 greenbc2 greenbcc redbc1 redbc2 redbcc irbc1 irbc2 irbcc
 do
 
@@ -36,13 +32,7 @@ do
   irbcc=$(printf "%d\n" "$irbcc")
 
   # Force integer-looking numbers to floats
-  for var in time serial session data_format firmware datetime offset ymd hms gps_lat gps_long gps_alt gps_speed gps_sat timebase status battery ax ay az tape flow_setpoint flow_total flow1 flow2 temp rh dew pressure int_temp opt_conf \
-  uv1 uv2 uvref uvatn1 uvatn2 uvk \
-  blue1 blue2 blueref blueatn1 blueatn2 bluek \
-  green1 green2 greenref greenatn1 greenatn2 greenk \
-  red1 red2 redref redatn1 redatn2 redk \
-  ir1 ir2 irref iratn1 iratn2 irk \
-  uvbc1 uvbc2 uvbcc bluebc1 bluebc2 bluebcc greenbc1 greenbc2 greenbcc redbc1 redbc2 redbcc irbc1 irbc2 irbcc
+  for var in flow_total flow1 flow2 bluebcc greenbcc irbcc redbcc uvbcc blueatn1 greenatn1 iratn1 redatn1 uvatn1
 do
   if [[ "${!var}" =~ ^-?[0-9]+$ ]]; then
     eval "$var=\"${!var}.0\""
@@ -50,7 +40,7 @@ do
 done
 
 
-  write_query="ma200,installation=$installation_name,instrument=$instrument_name flow_total=$flow_total,flow1=$flow1,flow2=$flow2,uv1=$uv1,uv2=$uv2,uvref=$uvref,uvatn1=$uvatn1,uvatn2=$uvatn2,uvk=$uvk,blue1=$blue1,blue2=$blue2,blueref=$blueref,blueatn1=$blueatn1,blueatn2=$blueatn2,bluek=$bluek,green1=$green1,green2=$green2,greenref=$greenref,greenatn1=$greenatn1,greenatn2=$greenatn2,greenk=$greenk,red1=$red1,red2=$red2,redref=$redref,redatn1=$redatn1,redatn2=$redatn2,redk=$redk,ir1=$ir1,ir2=$ir2,irref=$irref,iratn1=$iratn1,iratn2=$iratn2,irk=$irk,uvbc1=$uvbc1,uvbc2=$uvbc2,uvbcc=$uvbcc,bluebc1=$bluebc1,bluebc2=$bluebc2,bluebcc=$bluebcc,greenbc1=$greenbc1,greenbc2=$greenbc2,greenbcc=$greenbcc,redbc1=$redbc1,redbc2=$redbc2,redbcc=$redbcc,irbc1=$irbc1,irbc2=$irbc2,irbcc=$irbcc $timestamp_unix"
+  write_query="ma200,installation=$installation_name,instrument=$instrument_name flow_total=$flow_total,flow1=$flow1,flow2=$flow2,uvatn1=$uvatn1,blueatn1=$blueatn1,greenatn1=$greenatn1,redatn1=$redatn1,iratn1=$iratn1,uvbcc=$uvbcc,bluebcc=$bluebcc,greenbcc=$greenbcc,redbcc=$redbcc,irbcc=$irbcc $timestamp_unix"
 
   echo "$write_query" >> "$file_to_store"
 
