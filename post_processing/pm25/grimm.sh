11e81460da2d2a7a93c8097058907a10fa74ee31
Stasinos Konstantopoulos <konstant@iit.demokritos.gr>
2025-09-22 23:06:46 +0300

    Clean up unused PM2.5 scipts

--- post_processing/pm25/grimm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cat ${1}.txt | tr -d '\r' | sed 's|  *|,|g' > temp-input.grimm
-python3 grimm_aggregate.py
-cat temp-aggregated.csv | sed 's|-1||g' > temp-aggregated-clean.csv
-python3 grimm_mass.py > ${1}.csv
