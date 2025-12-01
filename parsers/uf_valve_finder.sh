03198db36f3a48f053de99c24e66810bbf84f296
Stasinos Konstantopoulos <konstant@iit.demokritos.gr>
2025-12-01 17:39:38 +0200

    Sync with 4m buffer
    Allow a 4m window around each valve state change
    during which measurements are invalid.

--- parsers/uf_valve_finder.sh
+++ parsers/uf_valve_finder.sh
@@ -14,39 +14,34 @@ cat "${file_to_process}" | tail +2 |\
      do
 	mytime=$(echo $line | cut -d , -f 252 | tr ' ' 'T')
 	q="import \"date\"
-	   t1=date.sub(d: 10m, from: $mytime)
+	   t1=date.sub(d: 3m, from: $mytime)
+	   t2=date.add(d: 2m, to: $mytime)
 	   from(bucket: \"mitrap006\")
-		|> range(start: t1, stop: $mytime) 
+		|> range(start: t1, stop: t2) 
 		|> filter(fn: (r) => r._measurement == \"uf\")
 		|> filter(fn: (r) => r._field == \"valve\")
-		|> last()
 		|> keep(columns: [\"_time\",\"_value\"])"
 	echo "Q1 : $q"
-	resp=$(influx query --raw --org mitrap --token $MITRAP_READ_TOKEN "$q" | awk '/^[^#]/ && /,/' | tail -1)
-	echo "R1: $resp"
-	#before_t=$(echo $resp | cut -d, -f 4)
-	before_v=$(echo $resp | cut -d, -f 5)
-	q="from(bucket: \"mitrap006\")
-	        |> range(start: $mytime, stop: now()) 
-		|> filter(fn: (r) => r._measurement == \"uf\")
-		|> filter(fn: (r) => r._field == \"valve\")
-		|> first()
-		|> keep(columns: [\"_time\",\"_value\"])"
-	echo "Q2: $q"
 	# Results have comments and empty lines.
-	# Good lines have , and do not start with #
-	# The first line is the header and the second is the CSV
-	# The CSV is ",result,table,_time,_value", so we need fields 4 and 5
-	resp=$(influx query --raw --org mitrap --token $MITRAP_READ_TOKEN "$q" | awk '/^[^#]/ && /,/' | tail -1)
-	echo "R2: $resp"
-	#after_t=$(echo $resp | cut -d, -f 4)
-	after_v=$(echo $resp | cut -d, -f 5)
-	if [[ $before_v == $after_v ]]; then
-		my_v=${before_v}
+	# Good lines have "," and do not start with "#"
+	# The CSV is ",result,table,_time,_value", so we need field 5
+	# influx client gives \r line-termination, change to space-separated
+	resp=$(influx query --raw --org mitrap --token $MITRAP_READ_TOKEN "$q" | awk '/^[^#]/ && /,/' | cut -d, -f 5 | sed 's|\r$| |g' )
+	# There is a header and the values, so if wc -l is 2 we have only one value,
+	# wc -l is 3 we have two values.
+	# Anything else is an error.
+	# Do not sort, if all is well there will be a series of 1's followed
+	# by a series of 0's (or vice versa), no mixing.
+	num_resp=$(echo $resp | tr ' ' '\n' | uniq | wc -l)
+	
+	if [[ x$num_resp == x2 ]]; then
+	    my_v=$(echo $resp | tr ' ' '\n' | tail -1)
+	elif [[ x$num_resp == x3 ]]; then
+	    my_v=2
 	else
-		#my_v=$(echo "($t0 - $t1)/($t2 - $t1)*$before_v + ($t2 - $t0)/($t2 - $t1)*$after_v" | bc -l )
-		my_v=2
+	    echo "ERROR"
 	fi
+	    
 	#echo "$line,$my_v,$before_t,$before_v,$after_v,$after_t" # DEBUG
 	echo "$line,$my_v" >> "${file_to_store}" 
      done) 
