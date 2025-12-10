2073308817ac508b634ee5445a49fdc050c00a3b
Stasinos Konstantopoulos <konstant@iit.demokritos.gr>
2025-12-10 20:06:02 +0200

    Update grafana panels
    Add category tags (Traffic, Port, etc.)
    Sync between online panels and templates
    after recent updates.

--- grafana/make.sh
+++ grafana/make.sh
@@ -13,20 +13,26 @@ prepare_query() {
 if [[ x$1 == xmitrap000 ]]; then
     TEMPLATE="CE_template.json"
     ID=6
-    INSTALLATION="Athens - Piraeus - CE"
-    TITLE="AQM-CE: Air Quality Monitoring, Cost Effective - Athens Piraeus"
+    STATION="Athens - Piraeus - CE"
+    TITLE="AQM-CE: Athens Piraeus"
+    CAMPAIGN="Athens"
+    CATEGORY="Port"
 elif  [[ x$1 == xmitrap001 ]]; then
     TEMPLATE="CE_template.json"
     ID=7
-    INSTALLATION="Athens - Aristotelous - CE"
-    TITLE="AQM-CE: Air Quality Monitoring, Cost Effective - Athens Aristotelous"
+    STATION="Athens - Aristotelous - CE"
+    TITLE="AQM-CE: Athens Aristotelous"
+    CAMPAIGN="Athens"
+    CATEGORY="Traffic"
 elif  [[ x$1 == xmitrap006 ]]; then
     TEMPLATE="HR_template.json"
     ID=3
-    INSTALLATION="Athens - Patission - HR"
-    TITLE="AQM-CE: Air Quality Monitoring, High Resolution - Athens Patission"
+    STATION="Athens - Patission - HR"
+    TITLE="AQM-HR: Athens Patission"
+    CAMPAIGN="Athens"
+    CATEGORY="Traffic"
 else
-    echo "Bad arg 1"
+    echo "bad arg 1"
     exit 1
 fi
 
@@ -48,6 +54,8 @@ fi
 
 cat ${TEMPLATE} |\
     sed "s|@ID_INT@|${ID}|g" |\
-    sed "s|@INSTALLATION_STR@|${INSTALLATION}|g" |\
+    sed "s|@STATION_STR@|${STATION}|g" |\
+    sed "s|@CAMPAIGN_STR@|${CAMPAIGN}|g" |\
+    sed "s|@CATEGORY_STR@|${CATEGORY}|g" |\
     sed "s|@TITLE_STR@|${TITLE}|g" > ${OUTFILE}
     
