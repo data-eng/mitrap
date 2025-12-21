c410541676edb89e01b9d5c08eb1dc15cbb56b9d
Stasinos Konstantopoulos <konstant@iit.demokritos.gr>
2025-12-21 23:19:04 +0200

    Consolidate valve parsers

--- parsers/valve.sh
+++ parsers/valve.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-escape_tag_value() {
-  local val="$1"
-  val="${val//\\/\\\\}"   # escape backslashes
-  val="${val//,/\\,}"     # escape commas
-  val="${val// /\\ }"     # escape spaces
-  echo "$val" | tr -cd '[:print:]' # remove funny codepoints
-}
+shopt -s lastpipe
 
 if [[ x"$5" == x ]]; then
   echo "Missing arguments: $*"
@@ -21,7 +15,56 @@ instrument_tz=$5
 
 temp=$(realpath "$0") && BINDIR=$(dirname "$temp")
 
-echo "ENV valve: $BINDIR $instrument_tz"
+TYPE1='^[12][0-9][0-9][0-9]-[01][0-9]-[0-3][0-9],[0-2][0-9]:[0-5][0-9]:[0-5][0-9],.*,[01],valve$'
+# Sample line:
+# 2025-05-12,00:00:26,14.54,PSI,100262.7,Pa,100.2698,kPa,752.129,torr,29.6121,inHg,0.989687,atm,1.002839,bar,8.9,%3,28.5,C3,32.9,%5,27.7,C5,0,valve
+# Found in Patission HR
 
-python3 ${BINDIR}/uf_valve.py "${file_to_process}" "${file_to_store}.csv" "${station_name}" "${instrument_name}" "${instrument_tz}" "other" > "${file_to_store}.lp"
+TYPE2='^[12][0-9][0-9][0-9]-[01][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9],.*,[01],valve,[0-9][0-9]*,fan$'
+# Sample line:
+# 2025-11-25 18:06:12, 14.42,PSI,98976.5,Pa,99.4378,kPa,745.855,torr,29.3646,inHg,0.981397,atm,0.990816,bar,67.1,%3,22.9,C3,29.9,%5,26.6,C5,0,valve,0,fan
+
+TYPE3='^[12][0-9][0-9][0-9]-[01][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9] b.nan,PSI,nan,Pa,nan,kPa,nan,torr,nan,inHg,nan,atm,nan,bar,0.0,%3,0.0,C3,0.0,%5,0.0,C5,[01],valve.*$'
+# Sample line:
+# 2025-12-20 00:01:40 b'nan,PSI,nan,Pa,nan,kPa,nan,torr,nan,inHg,nan,atm,nan,bar,0.0,%3,0.0,C3,0.0,%5,0.0,C5,0,valve\r\n'
+
+cat "${file_to_process}" | sed "s|${TYPE1}|1|" | sed "s|${TYPE2}|2|" | sed "s|${TYPE3}|3|" | sed 's|^...*$|0|' | sort | uniq -c |\
+	sed 's|^[[:space:]]*\([0-9][0-9]*\)[[:space:]][[:space:]]*\([0-9][0-0]*\)[[:space:]]*$|\1 \2|' |\
+	sort -rn | head -1 | cut -d ' ' -f 2 | read TYPE
+
+if [[ x$TYPE == x0 ]]; then
+	echo "valve: ERROR, unknown file type"
+	exit 1
+elif [[ x${TYPE} == x1 ]]; then
+	RE=$TYPE1
+	PROC=valve.py
+	ARG=1
+	cat "${file_to_process}" | grep -a "${RE}" > "${file_to_store}_temp1"
+elif [[ x${TYPE} == x2 ]]; then
+	RE=${TYPE2}
+	PROC=valve.py
+	ARG=2
+	cat "${file_to_process}" | grep -a "${RE}" > "${file_to_store}_temp1"
+elif [[ x${TYPE} == x3 ]]; then
+	RE=${TYPE3}
+	PROC=valve.py
+	ARG=3
+	cat "${file_to_process}" | grep -a "${RE}" | sed 's| b|,|' | sed 's|\\r\\n||' | tr -d \' > "${file_to_store}_temp1"
+else
+	echo "valve: Error"
+	exit 1
+fi
+
+cat "${file_to_process}" | grep -va "${RE}" | wc -l | read BADLINES
+
+echo "valve: ENV $BINDIR $instrument_tz PARSING as type ${TYPE} with ${BADLINES} bad lines"
+
+# Parse into csv
+python3 ${BINDIR}/${PROC} "${file_to_store}_temp1" "${file_to_store}_temp2" $ARG "${station_name}" "${instrument_name}" "${instrument_tz}" > "${file_to_store}.lp"
+
+# Calculate valve states that are too close to a valve position change to
+# be taken into account. These are marked with "2" instaead of 0/1.
+
+python3 ${BINDIR}/valve_state.py "${file_to_store}_temp2" "${file_to_store}.csv" 
+
 
--- parsers/valve_finder_offline.py
+++ parsers/valve_finder_offline.py
@@ -1,53 +1,55 @@
 import sys
 import pandas
-import numpy
 
-infile_measurement = sys.argv[1]
-infile_valve_state = sys.argv[2]
-outfile = sys.argv[3]
+infile = sys.argv[1]
+outfile = sys.argv[2]
+valvefile = sys.argv[3]
 
+dfd = pandas.read_csv( infile, parse_dates=["datetime"] )
+dfv = pandas.read_csv( valvefile, parse_dates=["datetime"], index_col="datetime" )
+names_in =["valve_state","valve2","valve3","valve5","valve7"] 
+names_out =["before","after","valve2","valve3","valve5","valve7"] 
 
-df = pandas.read_csv( infile, index_col="Sample #" )
+#idx=dfv.index.get_indexer([dfd.index[200]], method="nearest")
+#idx = 200
 
-##
-## DOES NOT WORK, WIP
-##
+def valve_finder( datetime, dfv, names ):
 
+    valve_before = dfv.index.get_indexer( [datetime], method="ffill" )
+    t0 = dfv.index[valve_before]
+    v0 = dfv.loc[t0,names]
 
-# Find the valve timepoints that box this timepoint
+    valve_after = dfv.index.get_indexer( [datetime], method="bfill" )
+    t1 = dfv.index[valve_after]
+    v1 = dfv.loc[t1,names]
+    dt = (t1.astype(int)-t0.astype(int))/1e9
 
-def helper( t1, t2 ):
-    if t1 > t2: return numpy.nan
-    else: return t2.value - t1.value
-def box( idx ):
-    t1 = min( df_valve.index, key=lambda x: helper(df_valve.loc[x,"datetime"],df.loc[idx,"datetime"]))
-    if t1 != t1:
-        # There is nothing to the left
-        return None,None,None,None
-    t2 = t1+1
-    if t2 < len(df_valve):
-        #return t1,df_valve.loc[t1,"datetime"],df_valve.loc[t1,"valve_state"],t2,df_valve.loc[t2,"datetime"],df_valve.loc[t2,"valve_state"]
-        return df_valve.loc[t1,"datetime"].value,df_valve.loc[t1,"valve_state"],df_valve.loc[t2,"datetime"].value,df_valve.loc[t2,"valve_state"]
-    else:
-        # The very last datapoint
-        return df_valve.loc[t1,"datetime"].value,df_valve.loc[t1,"valve_state"],df_valve.loc[t1,"datetime"].value,df_valve.loc[t1,"valve_state"]
+    retv = [v0.iloc[0,0],v1.iloc[0,0]]
+    for i in range(1,5):
+        if (v0.iloc[0,i]==2) or (v1.iloc[0,i]==2):
+            # Invalid value on either end, means invalid
+            v = 2
+        elif v0.iloc[0,i] == v1.iloc[0,i]:
+            # Both ends agree
+            v = v0.iloc[0,i]
+        else:
+            # 0->1 or 1->0 should never happen,
+            # there should always be a 2 between them
+            # But it can happen in edge cases of valve2, as the 
+            # safety margin is too short.
+            v = 2
+            assert i==0
+        # This should never happen
+        # If there is a gap, there should a 2
+        if v != 2 and dt>70:
+            assert 1==0
+        retv.append(v)
 
-def valve_frac( idx ):
-    t1,v1,t2,v2 = box( idx )
-    # There is nothing to the left
-    if v1!=v1: retv == None
-    # Avoid unnecessary calculations for most timepoints
-    # and also avoid the division by zero at the last timepoint
-    elif v1==v2: retv = v1
-    else:
-        p1 = float(df.loc[idx,"datetime"].value - t1) / (t2-t1)
-        p2 = float(t2 - df.loc[idx,"datetime"].value) / (t2-t1)
-        retv = p1*v1 + p2*v2
     return retv
 
-# We only provide the valve dataframe for off-line analytics.
-# For real-time viz, we use flux to find the valve fraction.
-if df_valve is not None:
-    df["valve_frac"] = df.index.to_series().apply(valve_frac)
-
+vv = dfd["datetime"].apply(valve_finder,args=[dfv,names_in]).tolist()
+vv = pandas.DataFrame( vv, columns=names_out )
+dfd = pandas.concat( [dfd,vv], axis=1 )
+dfd["num_meta_cols"] = dfd["num_meta_cols"].apply( lambda n: n+len(names_out) )
+dfd.set_index( "datetime" ).to_csv( outfile )
 
