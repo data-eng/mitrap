7a93d47a2fb3bbb43a92f5ac4ad4b3781bbeac1e
Stasinos Konstantopoulos <konstant@iit.demokritos.gr>
2026-03-27 12:36:15 +0200

    Extent mpss_lp_maker to select distribution
    Select if lp_maker uses interpolated or original
    data to make lp lines.
    New queries that use the valve tags to avoid joins.
    Show different scenarios on Hoek van Holland and
    Peretola HR boxes.

--- parsers/mpss_lp_maker.py
+++ parsers/mpss_lp_maker.py
@@ -11,9 +11,12 @@ df = pandas.read_csv( infile, parse_dates=["datetime"] )
 try: meas = sys.argv[2]
 except: meas = "mpss"
 
+try: values_prefix = sys.argv[3]
+except: values_prefix = "interp_nm"
+
 col = []
 for c in df.columns:
-    if c.startswith( "interp_nm" ): col.append( c )
+    if c.startswith( values_prefix ): col.append( c )
 
 for idx in df.index:
     stn = df.loc[idx,"station_name"].replace(" ","\\ ")
@@ -21,7 +24,8 @@ for idx in df.index:
 
     values = ""
     for c in col:
-        val_name = c.replace("interp_","")
+        if values_prefix == "nm": val_name = c 
+        else: val_name = c.replace( values_prefix, "nm" )
         if values == "": values = f"{val_name}={df.loc[idx,c]}"
         else: values += f",{val_name}={df.loc[idx,c]}"
     print( f"{meas},installation={stn},instrument={ins},valve={df.loc[idx,'valve_state']} {values} {df.loc[idx,'datetime'].value}" )
