0f33cc7f6e1bfeea35e3bfd323c1243bf9eb584f
Stasinos Konstantopoulos <konstant@iit.demokritos.gr>
2026-07-23 11:32:25 +0300

    Fix error in XACT parsing

--- parsers/inorg_xact.sh
+++ parsers/inorg_xact.sh
@@ -19,8 +19,6 @@ station_name=$3
 instrument_name=$4
 instrument_tz=$5
 
-instrument_tz="Europe/Amsterdam"
-
 temp=$(realpath "$0") && BINDIR=$(dirname "$temp")
 
 echo "ENV inorg_xact: $BINDIR $instrument_tz"
