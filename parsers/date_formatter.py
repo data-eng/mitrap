88aff1999b3d1b5b2ba174002594907f56ad8b61
Stasinos Konstantopoulos <konstant@iit.demokritos.gr>
2026-07-23 11:04:11 +0300

    Add Prague SMPS

--- parsers/date_formatter.py
+++ parsers/date_formatter.py
@@ -34,6 +34,9 @@ def make_tz( datetime_str, datetime_fmt, tz ):
 if datetime_fmt == "decimal":
     all_dt = [make_decimal(df.loc[i,date_col], df.loc[i,time_col], instrument_tz) for i in df.index]
     df = df.drop( ["Start Date","End Date","Start Year","End Year"], axis=1 )
+elif date_col == time_col:
+    all_dt = [make_utc(df.loc[i,date_col], datetime_fmt) for i in df.index]
+    df = df.drop( [date_col], axis=1 )
 else:
     all_dt = [make_utc(df.loc[i,date_col]+" "+df.loc[i,time_col], datetime_fmt) for i in df.index]
     df = df.drop( [date_col,time_col], axis=1 )
